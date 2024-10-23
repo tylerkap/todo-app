a255c1ba76fa4c16f7062495a1dbb74986c58041
Tyler Kap <tylerkap55@gmail.com>
2024-10-23 10:04:21 -0600

    Create List/Active List

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,82 @@
-let listArray = [];
+const testLists = {
+    1: {
+      name: "Shopping list",
+      todos: [
+        {
+          text: 'bananas',
+          completed: false
+        },
+        {
+          text: '1 lbs ground turkey',
+          completed: false
+        }
+      ]
+    },
+   }
 
-function addTask() {
-    let buttonValue = document.getElementById("taskInput").value;
-    let taskList = document.getElementById("taskList");
+const lists = {};
+const currentList = {};
 
-    listArray.push(buttonValue);
 
-    let li = document.createElement("li");
-    li.innerHTML = buttonValue;
-    taskList.appendChild(li);
+let listCount = 1;
+let listButton = document.getElementById('listButton');
+listButton.addEventListener('click', addNewList);
 
-    console.log(listArray);
+function renderPage() {
+    let sideBarContainer = document.getElementsByClassName('new-list-container')[0];
+    sideBarContainer.innerHTML = '';
+
+    for (const element in lists) {
+        let newList = document.createElement('div');
+        let listTitle = document.createElement('h1');
+        let title = document.createTextNode(lists[element].name);
+        newList.classList.add('list-item');
+        listTitle.appendChild(title);
+        newList.appendChild(listTitle);
+        sideBarContainer.appendChild(newList);
+
+        newList.addEventListener('click', () => {
+            let listItem = document.querySelectorAll('.list-item');
+            console.log(listItem);
+
+            for (let i = 0; i < listItem.length; i++) {
+                listItem[i].classList.remove('list-item-active')
+                console.log(listItem[i]);
+            }
+
+            newList.classList.toggle('list-item-active');
+            //activeList(newList);
+        });
+    }
 }
+
+function addNewList() {
+    
+    
+    let listInput = document.getElementById('list-name-input').value;
+
+    let listId = listCount;
+    if (listInput !== '' || listInput === undefined) {
+        lists[listId] = {name: listInput, todos: []};
+        console.log(lists);
+        
+        listCount++;
+        
+        renderPage();
+    }
+}
+
+function activeList(element) {
+    // let listItem = document.getElementsByClassName('list-item');
+    // for (let i = 0; listItem.length; i++) {
+    //     listItem[i].className = listItem[i].className.replace('list-item-active');
+    // }
+
+    // element.currentTarget.className += 'list-item-active';
+    // alert('hi');
+}
+
+
+
+
+
